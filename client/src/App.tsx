ad6ef3fe1c5765c46eee6317b246edd0f7446a01
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2025-07-06 00:52:12 +0300

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState<number>(0)
+
+  return (
+    <>
+      <button onClick={() => setCount(0)}>reset</button>
+      <button onClick={() => setCount(count + 1)}>increment</button>
+      <button onClick={() => setCount(count - 1)}>decrement</button>
+      <p>{count}</p>
+    </>
+  )
+}
+
+export default App
