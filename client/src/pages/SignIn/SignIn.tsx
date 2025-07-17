f5c197697db637bc89014c524079fe006d699082
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2025-07-18 01:17:36 +0300

    refactor(minor): navigate now uses paths.home as an argument

--- client/src/pages/SignIn/SignIn.tsx
+++ client/src/pages/SignIn/SignIn.tsx
@@ -5,16 +5,15 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import LockIcon from "@mui/icons-material/Lock";
 import Button from "@mui/material/Button";
 
-
-import type { SignInTypes } from "~/types/signInTypes";
-import { useSignInMutation } from "~/pages/SignIn/api/signInApi";
-
 import { useNavigate } from "react-router";
 
 import { useForm } from "react-hook-form";
 
-import styles from "~/pages/SignIn/SignIn.module.scss";
+import type { SignInTypes } from "~/types/signInTypes";
+import { useSignInMutation } from "~/pages/SignIn/api/signInApi";
+import { paths } from "~/routes/config";
 
+import styles from "~/pages/SignIn/SignIn.module.scss";
 
 function SignIn() {
   const { register, handleSubmit } = useForm<SignInTypes>();
@@ -33,7 +32,7 @@ function SignIn() {
     try {
       const result = await signIn(data).unwrap();
       console.log(result);
-      navigate("/");
+      navigate(paths.home);
     } catch {
       console.log(error);
     }
